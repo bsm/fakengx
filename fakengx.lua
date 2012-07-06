0e53d88a604d061fc278b92a29fa3c623cd0340c
Evgeniy Dolzhenko <dolzenko@gmail.com>
2012-07-06 11:56:28 +0100

    Add basic ngx.shared stubs

--- fakengx.lua
+++ fakengx.lua
@@ -192,6 +192,7 @@ function fakengx.new()
   ngx.header    = {}
   ngx.arg       = {}
   ngx.req       = {}
+  ngx.shared    = {}
 
   -- Internal Registries
   ngx._captures = Captures:new()
@@ -302,6 +303,17 @@ function fakengx.new()
 
   -- http://wiki.nginx.org/HttpLuaModule#ngx.req.read_body
   function ngx.req.read_body()
+  end
+
+  -- http://wiki.nginx.org/HttpLuaModule#ngx.shared.DICT.get
+  function ngx.shared.get(self, key)
+    return self[key]
+  end
+
+  -- http://wiki.nginx.org/HttpLuaModule#ngx.shared.DICT.set
+  function ngx.shared.set(self, key, value)
+    self[key] = value
+    return true
   end
 
   return ngx
